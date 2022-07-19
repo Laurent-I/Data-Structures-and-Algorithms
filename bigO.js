b5d52ae3c4fc55617b520037d94c5cd92deaffd1
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-19 11:48:21 +0200

    Added break for more efficiency and for the first rule of Big O notation

--- bigO.js
+++ bigO.js
@@ -1,23 +1,25 @@
-// const nemo = ['nemo']
-// const largeArray = new Array(10000).fill('nemo')
-//
-// function findNemo(array){
-//     for(let i = 0; i < array.length; i++){
-//         if(array[i] ==='nemo'){
-//             console.log('found NEMO!')
-//         }
-//     }
-// }
-//
-// findNemo(largeArray)
+const nemo = ['nemo']
+const largeArray = new Array(10000).fill('nemo')
+
+function findNemo(array){
+    for(let i = 0; i < array.length; i++){
+        console.log('Running');
+        if(array[i] ==='nemo'){
+            console.log('found NEMO!')
+            break;
+        }
+    }
+}
+
+findNemo(largeArray)
 
 //Constant Time Big Notation
 
-const boxes = [0,1,2,3,4,5];
-
-function logFirstTwoBoxes(boxes){
-    console.log(boxes[0]);//O(1)
-    console.log(boxes[1]);//O(1)
-}
-
-logFirstTwoBoxes(boxes);
+// const boxes = [0,1,2,3,4,5];
+//
+// function logFirstTwoBoxes(boxes){
+//     console.log(boxes[0]);//O(1)
+//     console.log(boxes[1]);//O(1)
+// }
+//
+// logFirstTwoBoxes(boxes);
