70714f26dd64dfed8d56ed8c90b6a2448208f0d2
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-19 12:22:28 +0200

    Â The Third Rule of Big O Notation about different terms

---
+++ ThirdRule.js
@@ -0,0 +1,13 @@
+function compressBoxesTwice(boxes, boxes2){
+    boxes.forEach(function(boxes){
+        console.log(boxes);
+    });
+
+    boxes2.forEach(function(boxes){
+        console.log(boxes);
+    });
+}
+//Addition for loops next to each other of different terms
+//O(a + b)
+//Multiplication for nested loops of different terms
+//O(a*b)
