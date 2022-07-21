4ef4b95ba4c9711d103aa0ec876ca1ecfabac91d
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-21 19:21:14 +0200

    Brute Force Solution on the First Array interview Question

--- Array/First_question/Question_1.js
+++ Array/First_question/Question_1.js
@@ -7,26 +7,26 @@
 //Step 5 : Double check errors
 
 // My trial actually!!!😅😅
-function findPair(array, target){
-
-    pairs = [];
-    for(let i=0; i<array.length; i++){
-        let p1 = array[i];
-        let number = target - p1;
-        for(let j=0; j<array.length; j++){
-            let p2 = number
-            if (p2 === array[j]){
-                pairs.push(p2, p1)
-
-            }
-
-        }
-    }
-
-    console.log(pairs) ;
-}
-
-findPair([1,3,7,9,2], 11)
+// function findPair(array, target){
+//
+//     pairs = [];
+//     for(let i=0; i<array.length; i++){
+//         let p1 = array[i];
+//         let number = target - p1;
+//         for(let j=0; j<array.length; j++){
+//             let p2 = number
+//             if (p2 === array[j]){
+//                 pairs.push(p2, p1)
+//
+//             }
+//
+//         }
+//     }
+//
+//     console.log(pairs) ;
+// }
+//
+// findPair([1,3,7,9,2], 11)
 
 ///////////////////////////////////////////
 //Brute Force Solution
@@ -34,23 +34,23 @@ findPair([1,3,7,9,2], 11)
 const numsArray = [1,3,7,9,2];
 const targetToFind = 11
 //
-// const findTwoSum = function(nums, target){
-//     for(let p1=0; p1<nums.length; p1++){
-//         console.log({p1, value: nums[p1]})
-//         const numberToFind = target - nums[p1];
-//         console.log({numberToFind})
-//         for(let p2 = p1 + 1; p2 < nums.length; p2++){
-//             console.log({p2, value: nums[p2]})
-//             if(numberToFind === nums[p2]){
-//                 return [p1, p2];
-//             }
-//         }
-//     }
-//
-//     return null;
-// }
-//
-// console.log(findTwoSum(numsArray, targetToFind))
+const findTwoSum = function(nums, target){
+    for(let p1=0; p1<nums.length; p1++){
+        console.log({p1, value: nums[p1]})
+        const numberToFind = target - nums[p1];
+        console.log({numberToFind})
+        for(let p2 = p1 + 1; p2 < nums.length; p2++){
+            console.log({p2, value: nums[p2]})
+            if(numberToFind === nums[p2]){
+                return [p1, p2];
+            }
+        }
+    }
+
+    return null;
+}
+
+console.log(findTwoSum(numsArray, targetToFind))
 
 //Step 6: Test using our test cases
 //Step 7: Space and Time Complexity of our code
