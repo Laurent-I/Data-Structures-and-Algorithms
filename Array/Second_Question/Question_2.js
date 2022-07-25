14272a0f208f70f2cffa3a3ffb86b62317cd852e
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-26 00:19:04 +0200

    Optimal Solution for the Max Area Water Container Question Using The two shifting pointers method

--- Array/Second_Question/Question_2.js
+++ Array/Second_Question/Question_2.js
@@ -10,48 +10,48 @@
 //Brute force solution
 
 let array = [7, 1, 2, 3, 9];
-
-const getMaxWaterContainer = function (heights) {
-  let maxArea = 0;
-  for (let p1 = 0; p1 < heights.length; p1++) {
-    for (let p2 = p1 + 1; p2 < heights.length; p2++) {
-      const height = Math.min(heights[p1], heights[p2]);
-      const width = p2 - p1;
-      const currentArea = height * width;
-      maxArea = Math.max(maxArea, currentArea);
-    }
-  }
-  return maxArea;
-};
-
-//time: O(n^2)
-//space: O(1)
-
-console.log(getMaxWaterContainer(array));
-
-//Optimal Solution
-// The two shifting pointers
+//
 // const getMaxWaterContainer = function (heights) {
-//   let p1 = 0;
-//   let p2 = heights.length - 1;
 //   let maxArea = 0;
-//
-//   while (p1 < p2) {
-//     const height = Math.min(heights[p1], heights[p2]);
-//     const width = p2 - p1;
-//     const currentArea = height * width;
-//     maxArea = Math.max(maxArea, currentArea);
-//
-//     if (heights[p1] <= heights[p2]) {
-//       p1++;
-//     } else {
-//       p2--;
+//   for (let p1 = 0; p1 < heights.length; p1++) {
+//     for (let p2 = p1 + 1; p2 < heights.length; p2++) {
+//       const height = Math.min(heights[p1], heights[p2]);
+//       const width = p2 - p1;
+//       const currentArea = height * width;
+//       maxArea = Math.max(maxArea, currentArea);
 //     }
 //   }
 //   return maxArea;
 // };
 //
+// //time: O(n^2)
+// //space: O(1)
+//
 // console.log(getMaxWaterContainer(array));
+
+//Optimal Solution
+// The two shifting pointers
+const getMaxWaterContainer = function (heights) {
+  let p1 = 0;
+  let p2 = heights.length - 1;
+  let maxArea = 0;
+
+  while (p1 < p2) {
+    const height = Math.min(heights[p1], heights[p2]);
+    const width = p2 - p1;
+    const currentArea = height * width;
+    maxArea = Math.max(maxArea, currentArea);
+
+    if (heights[p1] <= heights[p2]) {
+      p1++;
+    } else {
+      p2--;
+    }
+  }
+  return maxArea;
+};
+
+console.log(getMaxWaterContainer(array));
 
 //time: O(n)
 //space: O(1)
