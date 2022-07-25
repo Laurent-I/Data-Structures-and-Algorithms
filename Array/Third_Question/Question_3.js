27924ffe2bcc70a5321a035baa4a29523961f951
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-26 00:21:49 +0200

    Optimal Solution for the Trapping Rainwater Question Using The Two Pointer Method

--- Array/Third_Question/Question_3.js
+++ Array/Third_Question/Question_3.js
@@ -7,29 +7,29 @@
 //Brute Force Solution
 let array = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2];
 //
-const getTrappedRainwater = function (heights) {
-  let totalWater = 0;
-  for (let p = 0; p < heights.length; p++) {
-    let leftP = p,
-      rightP = p,
-      maxLeft = 0,
-      maxRight = 0;
-    while (leftP >= 0) {
-      maxLeft = Math.max(maxLeft, heights[leftP]);
-      leftP--;
-    }
-    while (rightP < heights.length) {
-      maxRight = Math.max(maxRight, heights[rightP]);
-      rightP++;
-    }
-    const currentWater = Math.min(maxLeft, maxRight) - heights[p];
-    if (currentWater >= 0) {
-      totalWater += currentWater;
-    }
-  }
-  return totalWater;
-};
-console.log(getTrappedRainwater(array));
+// const getTrappedRainwater = function (heights) {
+//   let totalWater = 0;
+//   for (let p = 0; p < heights.length; p++) {
+//     let leftP = p,
+//       rightP = p,
+//       maxLeft = 0,
+//       maxRight = 0;
+//     while (leftP >= 0) {
+//       maxLeft = Math.max(maxLeft, heights[leftP]);
+//       leftP--;
+//     }
+//     while (rightP < heights.length) {
+//       maxRight = Math.max(maxRight, heights[rightP]);
+//       rightP++;
+//     }
+//     const currentWater = Math.min(maxLeft, maxRight) - heights[p];
+//     if (currentWater >= 0) {
+//       totalWater += currentWater;
+//     }
+//   }
+//   return totalWater;
+// };
+// console.log(getTrappedRainwater(array));
 
 // Space = O(1)
 // Time = O(n^2)
@@ -43,33 +43,33 @@ console.log(getTrappedRainwater(array));
 // 3) move pointer inwards
 // 4) repeat for the other pointer
 
-// const getTrappedRainwater = function (heights) {
-//   let left = 0,
-//     right = heights.length - 1,
-//     leftMax = 0,
-//     rightMax = 0,
-//     total = 0;
-//   while (left < right) {
-//     if (heights[left] <= heights[right]) {
-//       if (heights[left] >= leftMax) {
-//         leftMax = heights[left];
-//       } else {
-//         total += leftMax - heights[left];
-//       }
-//       left++;
-//     } else {
-//       if (heights[right] >= rightMax) {
-//         rightMax = heights[right];
-//       } else {
-//         total += rightMax - heights[right];
-//       }
-//       right--;
-//     }
-//   }
-//   return total;
-// };
-//
-// console.log(getTrappedRainwater(array));
+const getTrappedRainwater = function (heights) {
+  let left = 0,
+    right = heights.length - 1,
+    leftMax = 0,
+    rightMax = 0,
+    total = 0;
+  while (left < right) {
+    if (heights[left] <= heights[right]) {
+      if (heights[left] >= leftMax) {
+        leftMax = heights[left];
+      } else {
+        total += leftMax - heights[left];
+      }
+      left++;
+    } else {
+      if (heights[right] >= rightMax) {
+        rightMax = heights[right];
+      } else {
+        total += rightMax - heights[right];
+      }
+      right--;
+    }
+  }
+  return total;
+};
+
+console.log(getTrappedRainwater(array));
 
 // Space = O(1)
 // Time = O(n)
