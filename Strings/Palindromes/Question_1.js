9c71f80226f165c8f611898a3e887ad31089d754
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-30 04:37:51 +0200

    Solving the Valid Palindrome question using the two pointer techinque

--- Strings/Palindromes/Question_1.js
+++ Strings/Palindromes/Question_1.js
@@ -5,17 +5,17 @@
 // Example: " A man, a plan, a canal: Panama" returns True
 // Identify some test cases
 
-// const isValidPalindrome = function (S) {
-//   S = S.replace(/[^A-Za-z\d]/g, "").toLowerCase();
-//   let p1 = 0;
-//   let p2 = S.length - 1;
-//   while (p1 < p2) {
-//     if (S[p1] !== S[p2]) {
-//       return false;
-//     }
-//     p1++;
-//     p2--;
-//   }
-//   return true;
-// };
-// console.log(isValidPalindrome("A man, a plan, a canal: Panama"));
+const isValidPalindrome = function (S) {
+  S = S.replace(/[^A-Za-z\d]/g, "").toLowerCase();
+  let p1 = 0;
+  let p2 = S.length - 1;
+  while (p1 < p2) {
+    if (S[p1] !== S[p2]) {
+      return false;
+    }
+    p1++;
+    p2--;
+  }
+  return true;
+};
+console.log(isValidPalindrome("A man, a plan, a canal: Panama"));
