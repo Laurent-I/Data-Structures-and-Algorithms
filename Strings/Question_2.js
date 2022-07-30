39880872b4cb19b441faff65098673652cef132f
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-30 04:34:44 +0200

    Brute Force Solution for the Longest Substring without repeat Question

--- Strings/Question_2.js
+++ Strings/Question_2.js
@@ -6,29 +6,29 @@
 // substring or subsequence
 
 ////////////////////Brute Force Solution/////////////////////////
-// const lengthOfLongestSubstring = function (S) {
-//   if (S.length <= 1) {
-//     return S.length;
-//   }
-//   let longest = 0;
-//   for (let left = 0; left < S.length; left++) {
-//     let seenChars = {},
-//       currentLength = 0;
-//     for (let right = left; right < S.length; right++) {
-//       const currentChar = S[right];
-//       if (!seenChars[currentChar]) {
-//         currentLength++;
-//         seenChars[currentChar] = true;
-//         longest = Math.max(longest, currentLength);
-//       } else {
-//         break;
-//       }
-//     }
-//   }
-//   return longest;
-// };
-//
-// console.log(lengthOfLongestSubstring("abccbda"));
+const lengthOfLongestSubstring = function (S) {
+  if (S.length <= 1) {
+    return S.length;
+  }
+  let longest = 0;
+  for (let left = 0; left < S.length; left++) {
+    let seenChars = {},
+      currentLength = 0;
+    for (let right = left; right < S.length; right++) {
+      const currentChar = S[right];
+      if (!seenChars[currentChar]) {
+        currentLength++;
+        seenChars[currentChar] = true;
+        longest = Math.max(longest, currentLength);
+      } else {
+        break;
+      }
+    }
+  }
+  return longest;
+};
+
+console.log(lengthOfLongestSubstring("abccbda"));
 // Time = O(n^2)
 // Space = O(n)
 
