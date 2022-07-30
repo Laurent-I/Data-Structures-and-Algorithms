d3357a0b1ed9d180bc3e1de8b56047aecc1e81b9
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-30 04:33:01 +0200

    Optimal  solution for the Backspace String Compare Question

--- Strings/Question_1.js
+++ Strings/Question_1.js
@@ -49,43 +49,43 @@ console.log(backspaceCompare("Ab#z", "ab#z"));
 
 ////////////////////Optimal Solution /////////////////////////
 
-// const backspaceCompare = function (S, T) {
-//   let p1 = S.length - 1;
-//   let p2 = T.length - 1;
-//   while (p1 >= 0 || p2 >= 0) {
-//     if (S[p1] === "#" || T[p2] === "#") {
-//       if (S[p1] === "#") {
-//         let backCount = 2;
-//         while (backCount > 0) {
-//           p1--;
-//           backCount--;
-//           if (S[p1] === "#") {
-//             backCount = backCount + 2;
-//           }
-//         }
-//       }
-//       if (T[p2] === "#") {
-//         let backCount = 2;
-//         while (backCount > 0) {
-//           p2--;
-//           backCount--;
-//           if (T[p2] === "#") {
-//             backCount = backCount + 2;
-//           }
-//         }
-//       }
-//     } else {
-//       if (S[p1] !== T[p2]) {
-//         return false;
-//       } else {
-//         p1--;
-//         p2--;
-//       }
-//     }
-//   }
-//   return true;
-// };
-// console.log(backspaceCompare("ab#z", "ac#z"));
+const OptimalBackspaceCompare = function (S, T) {
+  let p1 = S.length - 1;
+  let p2 = T.length - 1;
+  while (p1 >= 0 || p2 >= 0) {
+    if (S[p1] === "#" || T[p2] === "#") {
+      if (S[p1] === "#") {
+        let backCount = 2;
+        while (backCount > 0) {
+          p1--;
+          backCount--;
+          if (S[p1] === "#") {
+            backCount = backCount + 2;
+          }
+        }
+      }
+      if (T[p2] === "#") {
+        let backCount = 2;
+        while (backCount > 0) {
+          p2--;
+          backCount--;
+          if (T[p2] === "#") {
+            backCount = backCount + 2;
+          }
+        }
+      }
+    } else {
+      if (S[p1] !== T[p2]) {
+        return false;
+      } else {
+        p1--;
+        p2--;
+      }
+    }
+  }
+  return true;
+};
+console.log(OptimalBackspaceCompare("ab#z", "ac#z"));
 
 // Time: O(a + b)
 // Space: O(1)
