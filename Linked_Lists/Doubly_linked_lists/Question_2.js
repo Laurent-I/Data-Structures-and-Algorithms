c4133d8dffc13fac65d9806f62524f0c349c7247
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-07 01:34:19 +0200

    Detecting Cycles using Floyd's Tortoise and Hare Algorithm

--- Linked_Lists/Doubly_linked_lists/Question_2.js
+++ Linked_Lists/Doubly_linked_lists/Question_2.js
@@ -21,30 +21,30 @@ findCycle();
 /////////////////////////////////////////////////////////////////
 // Using Floyd's Tortoise and Hare Algorithm
 
-// const findCycle = function (head) {
-//   let hare = head,
-//     tortoise = head;
-//
-//   while (true) {
-//     hare = hare.next;
-//     tortoise = tortoise.next;
-//
-//     if (hare === null || hare.next === null) {
-//       return false;
-//     } else {
-//       hare = hare.next;
-//     }
-//     if (tortoise === hare) break;
-//   }
-//   let p1 = head,
-//     p2 = tortoise;
-//   while (p1 !== p2) {
-//     p1 = p1.next;
-//     p2 = p2.next;
-//   }
-//   return p1;
-// };
-// findCycle();
+const findCycleTH = function (head) {
+  let hare = head,
+    tortoise = head;
+
+  while (true) {
+    hare = hare.next;
+    tortoise = tortoise.next;
+
+    if (hare === null || hare.next === null) {
+      return false;
+    } else {
+      hare = hare.next;
+    }
+    if (tortoise === hare) break;
+  }
+  let p1 = head,
+    p2 = tortoise;
+  while (p1 !== p2) {
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+  return p1;
+};
+findCycleTH();
 
 // Space O(1)
 // Time O(n)
