ea3a7cbd543b6fdb4b497688ac53ff0987b37e32
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-10 00:33:09 +0200

    Minimum Brackets To Remove To Make Valid Stacks

--- Stacks_and_queues/Stacks/Question_2.js
+++ Stacks_and_queues/Stacks/Question_2.js
@@ -5,27 +5,27 @@
 
 // "a)bc(d)"
 
-// const minBracketsToRemove = function (str) {
-//   const res = str.split("");
-//   const stack = [];
-//
-//   for (let i = 0; i < res.length; i++) {
-//     if (res[i] === "(") {
-//       stack.push(i);
-//     } else if (res[i] === ")" && stack.length) {
-//       stack.pop();
-//     } else if (res[i] === ")") {
-//       res[i] = "";
-//     }
-//   }
-//   while (stack.length) {
-//     const currentIndex = stack.pop();
-//     res[currentIndex] = "";
-//   }
-//   return res.join("");
-// };
-//
-// console.log(minBracketsToRemove("a)bc(d)"));
+const minBracketsToRemove = function (str) {
+  const res = str.split("");
+  const stack = [];
+
+  for (let i = 0; i < res.length; i++) {
+    if (res[i] === "(") {
+      stack.push(i);
+    } else if (res[i] === ")" && stack.length) {
+      stack.pop();
+    } else if (res[i] === ")") {
+      res[i] = "";
+    }
+  }
+  while (stack.length) {
+    const currentIndex = stack.pop();
+    res[currentIndex] = "";
+  }
+  return res.join("");
+};
+
+console.log(minBracketsToRemove("a)bc(d)"));
 
 // Space: O(n)
 // Time O(n)
