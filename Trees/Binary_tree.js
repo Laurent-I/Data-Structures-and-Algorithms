193e551f6f83eeb34894e0515ab903fbbb19669f
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-12 08:54:11 +0200

    Binary Tree CheatsheetÂ

---
+++ Trees/Binary_tree.js
@@ -0,0 +1,14 @@
+// function BinaryTreeNode(value) {
+//   this.value = value;
+//   this.left = null;
+//   this.right = null;
+// }
+
+// Calculating nodes at level
+// Level 0: 2^0= 1
+// Level 1: 2^1= 2
+// Level 2: 2^2= 4
+// Level 3: 2^3= 8
+
+// number of nodes = 2^height - 1
+// log nodes = height
