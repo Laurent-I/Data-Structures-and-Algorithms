bf31534da15ab5567432a168b9a7680452808232
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-14 09:47:10 +0200

    Added insert method and node class for binary tree

--- Trees/Binary_tree.js
+++ Trees/Binary_tree.js
@@ -12,3 +12,59 @@
 
 // number of nodes = 2^height - 1
 // log nodes = height
+
+//Binary Search Tree Node
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+//Binary Search Tree Class
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  // function to be implemented
+  // insert(data)
+  // remove(data)
+
+  // Helper function
+  // findMinNode()
+  // getRootNode()
+  // inorder(node)
+  // preorder(node)
+  // postorder(node)
+  // search(node, data)
+
+  insert(data) {
+    // Initialise new node with the node class
+    let newNode = new Node(data);
+
+    if (this.root == null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, data);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left == null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right == null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+}
