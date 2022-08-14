f4706bc219a8d582b9bd322470d6292babf9a8aa
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-14 10:08:41 +0200

    Added remove method to the binary tree Class

--- Trees/Binary_tree.js
+++ Trees/Binary_tree.js
@@ -45,17 +45,21 @@ class BinarySearchTree {
     // Initialise new node with the node class
     let newNode = new Node(data);
 
+    // if root is null then the newNode becomes the root
     if (this.root == null) {
       this.root = newNode;
+      //If the root is not null call the insertNode method for proper insertion of the node
     } else {
       this.insertNode(this.root, data);
     }
   }
 
   insertNode(node, newNode) {
+    // if the newNode is less than the root or current node then we move left until we find a null space to insert the new node
     if (newNode.data < node.data) {
       if (node.left == null) {
         node.left = newNode;
+        // we recur until we find a null value
       } else {
         this.insertNode(node.left, newNode);
       }
@@ -65,6 +69,43 @@ class BinarySearchTree {
       } else {
         this.insertNode(node.right, newNode);
       }
+    }
+  }
+  remove(data) {
+    // root is re-initialized with
+    // root of a modified tree.
+    this.root = this.removeNode(this.root, data);
+  }
+
+  removeNode(node, key) {
+    if (node == null) {
+      return null;
+    } else if (key < node.data) {
+      node.left = this.removeNode(node.left, key);
+      return node;
+    } else if (key > node.data) {
+      node.right = this.removeNode(node.right, key);
+    } else {
+      if (node.left == null && node.right == null) {
+        node = null;
+        return node;
+      }
+      if (node.left == null) {
+        node = node.right;
+        return node;
+      } else if (node.right == null) {
+        node = node.left;
+        return node;
+      }
+      // Deleting node with two children
+      // minimum node of the right subtree
+      // is stored in aux
+
+      let aux = this.findMinNode(node.right);
+      node.data = aux.data;
+
+      node.right = this.removeNode(node.right, aux.data);
+      return node;
     }
   }
 }
